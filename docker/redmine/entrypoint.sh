be6d8e014bee8477d639e8f762710cc71505ab4f
Manuel <manuchichi@hotmail.com>
2021-01-21 19:38:36 -0300

    Fixed Images. Added logging in Nginx.

--- docker/redmine/entrypoint.sh
+++ docker/redmine/entrypoint.sh
@@ -3,4 +3,4 @@
 bundle exec rake generate_secret_token
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake redmine:load_default_data RAILS_ENV=production REDMINE_LANG=en
-bundle exec rake assets:precompile
+bundle exec puma -C config/puma.rb
